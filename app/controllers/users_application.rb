68231db4a5378d8e97b6b5a12642194e0c691e8f
the littlest schnauzer <thelittlestschnauzer@gmail.com>
2018-02-15 10:19:18 -0500

    created tables for user and books

--- app/controllers/users_application.rb
+++ app/controllers/users_application.rb
@@ -5,6 +5,17 @@ class UsersController < ApplicationController
       erb :'users/signup'
     else
       redirect to '/'
+    end
+  end
+
+  post '/signup' do
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
+      redirect to '/signup'
+    else
+      @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to '/'
     end 
   end
 end
